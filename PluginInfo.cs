345d8f7d1452fa8287c181db3da44fc237e014eb
kitkat5453 <charliereich111@gmail.com>
2025-06-01 20:38:07 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.kitkat.gorillatag.katpad";
+        public const string Name = "Kat Pad";
+        public const string Description = "The basic mod menu.";
+        public const string Version = "1.0.0";
+    }
+}
